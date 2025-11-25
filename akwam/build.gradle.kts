6c749dca197843380671e26069d59836ab040047
kim20598 <ki688494@gmail.com>
2025-11-25 23:19:24 +0100

    Update build.gradle.kts

--- akwam/build.gradle.kts
+++ akwam/build.gradle.kts
@@ -1,6 +1,11 @@
 // use an integer for version numbers
 version = 4
 
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
 cloudstream {
     language = "ar"
     authors = listOf("kim20598")
@@ -8,14 +13,30 @@ cloudstream {
     tvTypes = listOf(
         "Movie",
         "TvSeries"
-        // Add other types as needed
     )
-    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://ak.sv&size=%size%"
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&amp;type=FAVICON&amp;fallback_opts=TYPE,SIZE,URL&amp;url=https://ak.sv&amp;size=%size%"
     isCrossPlatform = true
+}
+
+android {
+    namespace = "com.akwam"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
     val implementation by configurations
-    // Add utils module dependency for SIMKL integration
     implementation(project(":utils"))
 }
