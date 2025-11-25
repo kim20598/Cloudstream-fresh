54cd2ff171036e070492eb2056b91b21e0712ee3
kim20598 <ki688494@gmail.com>
2025-11-25 21:48:08 +0100

    Update build.gradle.kts

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -17,14 +17,21 @@ android {
     }
     
     defaultConfig {
-        // Read SIMKL config from local.properties
-        val properties = java.util.Properties()
-        val localProperties = rootProject.file("local.properties")
-        if (localProperties.exists()) {
-            properties.load(localProperties.inputStream())
-            buildConfigField("String", "SIMKL_CLIENT_ID", "\"${properties.getProperty("SIMKL_CLIENT_ID", "")}\"")
-        } else {
-            buildConfigField("String", "SIMKL_CLIENT_ID", "\"c01381d4983a1830e7eaef34217e680a9371baa86c99ec08dd13b37a772125cb\"")
-        }
+        // Use direct SIMKL client ID without local.properties for now
+        buildConfigField("String", "SIMKL_CLIENT_ID", "\"c01381d4983a1830e7eaef34217e680a9371baa86c99ec08dd13b37a772125cb\"")
     }
+}
+
+dependencies {
+    val implementation by configurations
+    val cloudstream by configurations
+
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+    implementation("com.github.Blatzar:NiceHttp:0.4.13")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jsoup:jsoup:1.21.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
