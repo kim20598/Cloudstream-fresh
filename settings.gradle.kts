c7f593e94c614f37a73207ee71e3e12b968d8f3a
kim20598 <ki688494@gmail.com>
2025-11-25 21:36:07 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,5 @@
 rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included.
-// All new projects should get automatically included unless specified in the "disabled" variable.
-
 val disabled = listOf<String>()
 
 File(rootDir, ".").eachDir { dir ->
@@ -15,5 +12,5 @@ fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
 
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+// Ensure utils is included
+include("utils")
