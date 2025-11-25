eee9b229d48f36e68b0e02cc3dfc0720c1fbb2bf
kim20598 <ki688494@gmail.com>
2025-11-25 23:37:28 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,10 @@ rootProject.name = "CloudstreamPlugins"
 
 val disabled = listOf<String>()
 
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
+
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
         include(dir.name)
