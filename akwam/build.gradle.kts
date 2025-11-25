db92dc1efff081fe4cdac4e9b8c40e3438a0948f
kim20598 <ki688494@gmail.com>
2025-11-25 23:48:16 +0100

    Update build.gradle.kts

--- akwam/build.gradle.kts
+++ akwam/build.gradle.kts
@@ -14,26 +14,19 @@ cloudstream {
         "Movie",
         "TvSeries"
     )
-    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&amp;type=FAVICON&amp;fallback_opts=TYPE,SIZE,URL&amp;url=https://ak.sv&amp;size=%size%"
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://ak.sv&size=%size%"
     isCrossPlatform = true
 }
 
 android {
     namespace = "com.akwam"
     compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-
+    defaultConfig { minSdk = 21 }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    kotlinOptions { jvmTarget = "1.8" }
 }
 
 dependencies {
