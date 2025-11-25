533f6dafac8acee5cda10c044eeff47c29be8772
kim20598 <ki688494@gmail.com>
2025-11-25 22:02:24 +0100

    Update build.gradle.kts

--- egydead/build.gradle.kts
+++ egydead/build.gradle.kts
@@ -1,27 +1,21 @@
 // use an integer for version numbers
 version = 4
 
-
 cloudstream {
     language = "ar"
-    // All of these properties are optional, you can safely remove them
-
     authors = listOf("kim20598")
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
-    status = 1 // will be 3 if unspecified
+    status = 1
     tvTypes = listOf(
         "Movie",
         "TvSeries"
+        // Add other types as needed
     )
-
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://egydead.skin&size=%size%"
-
     isCrossPlatform = true
+}
+
+dependencies {
+    val implementation by configurations
+    // Add utils module dependency for SIMKL integration
+    implementation(project(":utils"))
 }
